8db9a6dfefcaab3c8f8ba5f62d644f1c84466068
kekcoleg69 <kekcoleg69@gmail.com>
2025-05-11 16:00:43 +0300

    module9-task1

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,7 @@
-/* в этот файл добавляет скрипты*/
+const toggleBtn = document.querySelector('.main-nav__toggle');
+const navMenu = document.querySelector('.main-nav');
+
+toggleBtn.onclick = function(){
+  toggleBtn.classList.toggle('main-nav__toggle--open');
+  navMenu.classList.toggle('main-nav--opened');
+};
