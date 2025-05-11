b87e89cc2071819a5be0b53434cb357caf23c072
kekcoleg69 <kekcoleg69@gmail.com>
2025-05-11 13:01:11 +0000

    Deploying to gh-pages from @ kekcoleg69/2565795-cat-energy-2@8db9a6dfefcaab3c8f8ba5f62d644f1c84466068 🚀

---
+++ scripts/index.js
@@ -0,0 +1,1 @@
+var n=document.querySelector(".main-nav__toggle"),e=document.querySelector(".main-nav");n.onclick=function(){n.classList.toggle("main-nav__toggle--open"),e.classList.toggle("main-nav--opened")};
