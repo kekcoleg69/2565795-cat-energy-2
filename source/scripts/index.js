13ca8ca97f9518f931a2b714c676183f7df95cbe
kekcoleg69 <kekcoleg69@gmail.com>
2025-05-20 09:23:00 +0300

    исправление ошибок.Карта

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -5,3 +5,25 @@ toggleBtn.onclick = function(){
   toggleBtn.classList.toggle('main-nav__toggle--open');
   navMenu.classList.toggle('main-nav--opened');
 };
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  const ymaps = window.ymaps;
+  ymaps.ready(() => {
+    const myMap = new ymaps.Map('map', {
+        center: [59.938679, 30.3230044],
+        zoom: 14,
+        controls: []
+      }),
+
+      myPlacemark = new ymaps.Placemark(myMap.getCenter(), {}, {
+        iconLayout: 'default#image',
+        iconImageHref: '../images/cooperation-map-pin@1x.webp',
+        iconImageSize: [57, 53],
+        iconImageOffset: [-26, -42]
+      });
+
+    myMap.geoObjects.add(myPlacemark);
+  });
+
+});
