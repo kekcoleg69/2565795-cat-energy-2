badb3842acf611f62938d23dcaf2c52fad5e7939
kekcoleg69 <kekcoleg69@gmail.com>
2025-06-01 23:18:23 +0300

    Исправления

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -15,17 +15,30 @@ document.addEventListener('DOMContentLoaded', () => {
   const ymaps = window.ymaps;
   ymaps.ready(() => {
     const myMap = new ymaps.Map('map', {
-        center: [59.938679, 30.3230044],
-        zoom: 14,
-        controls: []
-      }),
+      center: [59.938679, 30.3230044],
+      zoom: 14,
+      controls: []
+    });
 
-      myPlacemark = new ymaps.Placemark(myMap.getCenter(), {}, {
-        iconLayout: 'default#image',
-        iconImageHref: '../images/cooperation-map-pin@1x.webp',
-        iconImageSize: [57, 53],
-        iconImageOffset: [-26, -42]
-      });
+    const screenWidth = window.innerWidth;
+
+    let iconSize;
+    let iconOffset;
+
+    if (screenWidth >= 768) {
+      iconSize = [113, 106];
+      iconOffset = [-56, -93];
+    } else {
+      iconSize = [57, 53];
+      iconOffset = [-26, -42];
+    }
+
+    const myPlacemark = new ymaps.Placemark(myMap.getCenter(), {}, {
+      iconLayout: 'default#image',
+      iconImageHref: '../images/cooperation-map-pin@2x.png',
+      iconImageSize: iconSize,
+      iconImageOffset: iconOffset
+    });
 
     myMap.geoObjects.add(myPlacemark);
   });
